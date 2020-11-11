e1451970fbc202887a589d883f606d7f97d2f980
asif-kamal <asif.kamal1489@gmail.com>
2020-11-10 20:57:18 -0500

    added renderImages

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
-const favActors = ["keanu reeves", "nicolas cage", "will smith"];
-  //create a function that will use the above array to generate buttons
-  // and append them to the page
-  // the function should take 3 arguements - 
-  //an array of strings, className to be applied to the buttons, and the id of an element to append the buttons to. The button text should match the strings in our array.
-function renderButtons(textArray, className, elementId){
-  textArray.forEach(string => {
-    const btn = createButton(string, className)
-    document.getElementById(elementId).append(btn)
-  });
-}
 
-function createButton(text, className){
-  let btn = document.createElement("button")
-  btn.innerText = text
-  btn.className = className
-  return btn
-}
 
-renderButtons(favActors, "actor-button", "button-container")
+document.addEventListener("DOMContentLoaded", function(){
+  const favActors = ["keanu reeves", "nicolas cage", "will smith"];
+  function renderButtons(textArray, className, elementId){
+    const container = document.getElementById(elementId)
+    container.innerHTML = ""
+    textArray.forEach(string => {
+      const btn = createButton(string, className)
+      container.append(btn)
+    });
+  }
+  function createButton(text, className){
+    let btn = document.createElement("button")
+    btn.innerText = text
+    btn.className = className
+    btn.style.margin =  "30px"
+    return btn
+  }
+  const container = document.getElementById("button-container")
+  container.addEventListener("click", function(event){
+    event.stopPropagation()
+    if(event.target.className === "actor-button"){
+      console.log("clicked: ")
+      fetchImages(event.target.innerText)
+    }
+  })
+
+  function renderImages(imagesArr){
+    console.log(imagesArr)
+    let imagesContainer = document.getElementById('images-container')
+    imagesContainer.innerHTML = ""
+    imagesArr.forEach(function(imageObj) {
+      const url = imageObj.images.fixed_height_small.url
+      let img = document.createElement('img')
+      img.src = url
+     imagesContainer.appendChild(img)
+    
+    })
+  }
+
+  function fetchImages(buttonText){
+    const url = `https://api.giphy.com/v1/gifs/search?q=${buttonText}&rating=G&api_key=dc6zaTOxFJmzC&limit=10`
+    fetch(url)
+    .then(resp => resp.json())
+    .then(json => renderImages(json.data))
+  }
+  function renderForm(){
+    const container = document.getElementById("form-container")
+    const form = `
+      <form id="actor-form">
+        <label for="actor">Add Another Favorite:</label>
+        <input type="text" name="actor" />
+        <input type="submit" value="Add" />
+      </form>
+    `
+    container.innerHTML = form
+  }
+  function attachSubmitListener(){
+    const form = document.getElementById("actor-form")
+    form.addEventListener("submit", function(event){
+      event.preventDefault()
+      console.log(event)
+      favActors.push(event.target.actor.value)
+      renderButtons(favActors, "actor-button", "button-container")
+    })
+  }
+  renderForm()
+  attachSubmitListener()
+  renderButtons(favActors, "actor-button", "button-container")
+})
+//- create an form with a single input. On submit, take the input value, add it too the favActors array and rerender the buttons to include the new text 
